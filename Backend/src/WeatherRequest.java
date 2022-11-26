d385a966a639056e92ea15764590b8e51a7c9b5a
Ari57 <njds7777@gmail.com>
2022-11-26 22:44:18 +0000

    Updated success and fail html files, created a Java class for API request

---
+++ Backend/src/WeatherRequest.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.net.*;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+
+public class WeatherRequest {
+    public static void getRequest(String urlToRead) throws Exception {
+        StringBuilder result = new StringBuilder();
+        URL url = new URL(urlToRead);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
+            for (String line; (line = reader.readLine()) != null;) {
+                result.append(line);
+            }
+        }
+        //JSONObject jsonObj = new JSONObject(result.toString());
+        JSONObject jsonObject = (JSONObject) JSONValue.parse(result.toString());
+        //return result.toString();
+        //System.out.println(jsonObject.get("alerts"));
+        Object results = jsonObject.get("current");
+        System.out.println(results);
+    }
+
+    public static void main(String[] args) throws Exception {
+        getRequest("https://api.openweathermap.org/data/3.0/onecall?lat=51.5072&lon=0.1276&appid={AppID}&units=metric");
+    }
+    
+}
--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser")
 const app = express()
 const port = 3000
 const {con: con} = require("./credentials")
+var path = require("path")
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false});
 
@@ -10,13 +11,20 @@ app.use(express.static('public'));
 
 
 app.get("/success", function (req, res) {
-    res.sendFile(__dirname + "/success.html")
+    res.sendFile(path.join(__dirname, '../public', 'success.html'));
+})
+
+app.get("/fail", function (req, res) {
+  res.sendFile(path.join(__dirname, '../public', 'fail.html'));
 })
 
 app.post('/location', urlencodedParser, function(req, res){
   var sql = "INSERT INTO APIDATA (Location) VALUES ('"+req.body.locationName+"')"
   con.query(sql, function (err, result) {
-    if (err) throw err
+    if (err) {
+      res.redirect("/fail")
+      throw err 
+    }
     console.log(req.body.locationName)
     res.redirect("/success")
   })
