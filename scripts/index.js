6a1a7d584adbae06feafb319cf784a5e894327ba
Ari57 <njds7777@gmail.com>
2022-12-10 14:05:14 +0000

    Created server side of socket, now need to create a Java client

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,20 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const app = express()
-const port = 3000
-const {con: con} = require("./credentials")
 var path = require("path")
+const socketio = require("socket.io")
+const server = app.listen(8080, () => {
+    console.log("Server is running")
+})
+const io = socketio(server)
+
+io.on("connection", (socket) => {
+    console.log("Connected to client")
+})
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false});
 
 app.use(express.static('public'));
-
-
 app.get("/success", function (req, res) {
     res.sendFile(path.join(__dirname, '../public', 'success.html'));
 })
@@ -18,19 +23,18 @@ app.get("/fail", function (req, res) {
   res.sendFile(path.join(__dirname, '../public', 'fail.html'));
 })
 
-app.post('/location', urlencodedParser, function(req, res){
-  var sql = "INSERT INTO APIDATA (Location) VALUES ('"+req.body.locationName+"')"
-  con.query(sql, function (err, result) {
-    if (err) {
-      res.redirect("/fail")
-      throw err 
-    }
-    console.log(req.body.locationName)
-    res.redirect("/success")
+app.post('/location', urlencodedParser, (req, res) => {
+
+    io.on("error", (error) => {
+    res.redirect("/fail")
+    throw err
   })
-})
+
+    io.emit("message", req.body.locationName)
+    res.redirect("/success")  
+  })
 
 
-app.listen(port, () => {
-    console.log(`App running on port ${port}`)
+app.listen(3000, () => {
+    console.log("App running on port 3000")
 })
